5df8156c17b725664e70d34d0a6a5ca3e3d91c9e
Jeff <jeff.hissem@gmail.com>
2014-03-06 21:10:06 -0700

    Initial Commit

---
+++ Mneme/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Mneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.ClipboardButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.ValueBox = new System.Windows.Forms.TextBox();
+            this.KeyBox = new System.Windows.Forms.TextBox();
+            this.KeyList = new System.Windows.Forms.ListBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.DeleteButton);
+            this.panel1.Controls.Add(this.StatusLabel);
+            this.panel1.Controls.Add(this.ClipboardButton);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.EditButton);
+            this.panel1.Controls.Add(this.SaveButton);
+            this.panel1.Controls.Add(this.ValueBox);
+            this.panel1.Controls.Add(this.KeyBox);
+            this.panel1.Location = new System.Drawing.Point(200, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(216, 213);
+            this.panel1.TabIndex = 0;
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(11, 191);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(122, 13);
+            this.StatusLabel.TabIndex = 7;
+            this.StatusLabel.Text = "Status: Nothing to report";
+            // 
+            // ClipboardButton
+            // 
+            this.ClipboardButton.Enabled = false;
+            this.ClipboardButton.Location = new System.Drawing.Point(14, 134);
+            this.ClipboardButton.Name = "ClipboardButton";
+            this.ClipboardButton.Size = new System.Drawing.Size(188, 23);
+            this.ClipboardButton.TabIndex = 6;
+            this.ClipboardButton.Text = "Copy to Clipboard";
+            this.ClipboardButton.UseVisualStyleBackColor = true;
+            this.ClipboardButton.Click += new System.EventHandler(this.ClipboardButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Value";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // EditButton
+            // 
+            this.EditButton.Enabled = false;
+            this.EditButton.Location = new System.Drawing.Point(141, 165);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(61, 23);
+            this.EditButton.TabIndex = 3;
+            this.EditButton.Text = "Edit";
+            this.EditButton.UseVisualStyleBackColor = true;
+            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Enabled = false;
+            this.SaveButton.Location = new System.Drawing.Point(8, 165);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(61, 23);
+            this.SaveButton.TabIndex = 2;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // ValueBox
+            // 
+            this.ValueBox.Location = new System.Drawing.Point(14, 81);
+            this.ValueBox.Name = "ValueBox";
+            this.ValueBox.ReadOnly = true;
+            this.ValueBox.Size = new System.Drawing.Size(188, 20);
+            this.ValueBox.TabIndex = 1;
+            this.ValueBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ValueBox_KeyDown);
+            // 
+            // KeyBox
+            // 
+            this.KeyBox.Location = new System.Drawing.Point(14, 30);
+            this.KeyBox.Name = "KeyBox";
+            this.KeyBox.ReadOnly = true;
+            this.KeyBox.Size = new System.Drawing.Size(188, 20);
+            this.KeyBox.TabIndex = 0;
+            // 
+            // KeyList
+            // 
+            this.KeyList.FormattingEnabled = true;
+            this.KeyList.Items.AddRange(new object[] {
+            "Add New Item..."});
+            this.KeyList.Location = new System.Drawing.Point(13, 13);
+            this.KeyList.Name = "KeyList";
+            this.KeyList.Size = new System.Drawing.Size(179, 212);
+            this.KeyList.TabIndex = 1;
+            this.KeyList.SelectedIndexChanged += new System.EventHandler(this.KeyList_SelectedIndexChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Enabled = false;
+            this.DeleteButton.Location = new System.Drawing.Point(75, 165);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(61, 23);
+            this.DeleteButton.TabIndex = 8;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 231);
+            this.Controls.Add(this.KeyList);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Mneme";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox ValueBox;
+        private System.Windows.Forms.TextBox KeyBox;
+        private System.Windows.Forms.ListBox KeyList;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ClipboardButton;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Button DeleteButton;
+    }
+}
+
---
+++ Mneme/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Mneme
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        bool editing = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Open XML document and populate dictionary, if it doesn't exist then create one
+            if (!File.Exists("data.xml"))
+            {
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+
+                using (XmlWriter writer = XmlWriter.Create("data.xml", settings))
+                {
+                    writer.WriteStartDocument();
+                    writer.WriteComment("This is an auto generated file, please don't mess around with it unless you know what you are doing");
+
+                    //Main Element
+                    writer.WriteStartElement("DataSets");
+
+                    //Sample
+                    writer.WriteStartElement("Data");
+                    writer.WriteAttributeString("Key", "(SAMPLE) Phone Number");
+                    writer.WriteValue("(123)555-1234");
+                    writer.WriteEndElement();
+
+                    writer.WriteEndDocument();
+                }
+            }
+
+            using (XmlReader reader = XmlReader.Create("data.xml"))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        string key = string.Empty;
+                        string value = string.Empty;
+                        switch (reader.Name)
+                        {
+                            case "Data":
+                                key = reader["Key"];
+                                if (reader.Read())
+                                {
+                                    value = reader.Value.Trim();
+                                }
+                                data.Add(key, value);
+                                break;
+                            default:
+                                break;
+                        }                        
+                    }
+                }
+            }
+
+
+            //populate ListBox
+            foreach (KeyValuePair<string, string> input in data)
+            {
+                KeyList.Items.Add(input.Key);
+            }
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        private void KeyList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (KeyList.SelectedIndex != -1)
+            {
+                KeyBox.Text = "";
+                ValueBox.Text = "";
+                StatusLabel.Text = "Status: Nothing to report";
+                if (KeyList.SelectedIndex == 0)
+                {
+                    EditButton.Enabled = false;
+                    DeleteButton.Enabled = false;
+                    ClipboardButton.Enabled = false;
+                    SaveButton.Enabled = true;
+                    KeyBox.ReadOnly = false;
+                    ValueBox.ReadOnly = false;
+
+                    KeyBox.Focus();
+                }
+                else
+                {
+                    try
+                    {
+                        KeyBox.Text = KeyList.Items[KeyList.SelectedIndex].ToString();
+                        ValueBox.Text = data[KeyList.Items[KeyList.SelectedIndex].ToString()];
+                        ValueBox.ReadOnly = true;
+                        SaveButton.Enabled = false;
+                        EditButton.Enabled = true;
+                        DeleteButton.Enabled = true;
+                        ClipboardButton.Enabled = true;
+                        ClipboardButton.Focus();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void ClipboardButton_Click(object sender, EventArgs e)
+        {
+            if (KeyList.SelectedIndex != -1 || KeyList.SelectedIndex != 0)
+            {
+                try
+                {
+                    System.Windows.Forms.Clipboard.SetText(data[KeyList.Items[KeyList.SelectedIndex].ToString()], TextDataFormat.Text);
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if (KeyBox.Text != "" || KeyBox.Text != null)
+            {
+                StatusLabel.Text = "Status: Saving...";
+                try
+                {
+                    if (!editing)
+                    {
+                        data.Add(KeyBox.Text, ValueBox.Text);
+                        KeyList.Items.Add(KeyBox.Text);
+                        KeyList.SelectedIndex = KeyList.Items.Count - 1;
+                    }
+                    else
+                    {
+                        data[KeyList.Items[KeyList.SelectedIndex].ToString()] = ValueBox.Text;
+                        editing = false;
+                    }
+                    saveXML();
+
+                    StatusLabel.Text = "Status: Saved!";
+
+                    EditButton.Enabled = true;
+                    DeleteButton.Enabled = true;
+                    ClipboardButton.Enabled = true;
+                    SaveButton.Enabled = false;
+                    KeyBox.ReadOnly = true;
+                    ValueBox.ReadOnly = true;                    
+                }
+                catch (ArgumentException) { StatusLabel.Text = "Status: This name already exists, please use a unique name"; }
+                catch (Exception ex) { StatusLabel.Text = "Status: " + ex.Message; }
+            }
+            else { StatusLabel.Text = "Status: Empty names are not allowed"; }
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            ValueBox.ReadOnly = false;
+            SaveButton.Enabled = true;
+            EditButton.Enabled = false;
+
+            editing = true;
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to remove \"" + KeyList.Items[KeyList.SelectedIndex].ToString() + "\"?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                try
+                {
+                    StatusLabel.Text = "Status: Removing...";
+                    data.Remove(KeyList.Items[KeyList.SelectedIndex].ToString());
+                    KeyList.Items.Remove(KeyList.Items[KeyList.SelectedIndex].ToString());
+                    saveXML();
+                    StatusLabel.Text = "Status: Removed!";
+                    KeyList.SelectedIndex = KeyList.Items.Count - 1;
+                }
+                catch (Exception ex) { StatusLabel.Text = "Status: " + ex.Message; }
+            }
+        }
+
+        private void saveXML()
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+
+            using (XmlWriter writer = XmlWriter.Create("data.xml", settings))
+            {
+                writer.WriteStartDocument();
+                writer.WriteComment("This is an auto generated file, please don't mess around with it unless you know what you are doing");
+
+                //Main Element
+                writer.WriteStartElement("DataSets");
+
+                foreach (KeyValuePair<string, string> input in data)
+                {
+                    writer.WriteStartElement("Data");
+                    writer.WriteAttributeString("Key", input.Key);
+                    writer.WriteValue(input.Value);
+                    writer.WriteEndElement();
+                }               
+
+                writer.WriteEndDocument();
+            }
+        }
+
+        private void ValueBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && ValueBox.Enabled == true)
+            {
+                SaveButton.PerformClick();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+    }
+}
